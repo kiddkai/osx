82bd4e0ac274c113a2c0bcbb9d7a35028027310f
kiddkai <kiddkai@gmail.com>
2016-05-30 10:28:07 +1000

    hooks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,12 @@
+;;; init.el -- initialize the whole emacs
+
+;;; Commentary:
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory
+
+;;; Code:
 (package-initialize)
 
 (require 'package)
@@ -43,21 +48,45 @@
   (add-hook 'org-mode-hook #'flyspell-mode))
 
 (use-package hydra)
+
 (use-package lua-mode
   :config
   (setq lua-indent-level 2))
-(use-package clojure-mode)
 (use-package smartparens)
 (use-package rainbow-delimiters)
 (use-package cider)
 (use-package js2-mode)
+
+(use-package cider)
 (use-package clj-refactor)
+(use-package clojure-mode)
+
+(defun my-clojure-mode-hook ()
+  "Custom clojure mode hook."
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (add-hook 'js-mode-hook #'smartparens-mode)
+(add-hook 'js-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
 (add-hook 'clojure-mode-hook #'smartparens-strict-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
-;;; init setting
+(defun cider-figwheel-repl ()
+  "Enable figwheel in cider repl."
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/start-figwheel!) ; idempotent
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
+
+
 (setq inhibit-startup-message t)
 
 (defconst *is-a-mac* (eq system-type 'darwin))
@@ -84,9 +113,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e24679edfdea016519c0e2d4a5e57157a11f928b7ef4361d00c23a7fe54b8e01" "db9feb330fd7cb170b01b8c3c6ecdc5179fc321f1a4824da6c53609b033b2810" "e2e4e109357cfcebccb17961950da6b84f72187ade0920a4494013489df648fe" "bf81a86f9cfa079a7bb9841bc6ecf9a2e8999b85e4ae1a4d0138975921315713" "d43120398682953ef18fd7e11e69c94e44d39bb2ab450c4e64815311542acbff" "cdfb22711f64d0e665f40b2607879fcf2607764b2b70d672ddaa26d2da13049f" default)))
  '(package-selected-packages
    (quote
-    (js2-mode rainbow-delimiters smartparens clj-refactor cider clojure-mode lua-mode hydra flycheck helm-projectile projectile magit helm use-package))))
+    (base16-theme js2-mode rainbow-delimiters smartparens clj-refactor cider clojure-mode lua-mode hydra flycheck helm-projectile projectile magit helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
